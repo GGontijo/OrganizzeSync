072ba1cb5fa3592821872588e3fe14681c692dce
Gabriel.Gontijo <38219914+GGontijo@users.noreply.github.com>
2023-07-10 23:00:43 -0400

    update

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def dev():
     print(b)
 
 if __name__ == "__main__":
-    #loop = asyncio.get_event_loop()
-    #loop.run_until_complete(main())
-    dev()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    #dev()
 
